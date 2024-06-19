80760e6e2ced2b18ffe3b9b90953ad3bb56c2ed0
SKY <sky012877@gmail.com>
2024-06-19 11:55:07 -0700

    src/settings.rs - Added :

---
+++ src/settings.rs
@@ -0,0 +1,16 @@
+// use crate::project::AppSettings;     
+
+// // === Whole Project setting file ===
+
+// pub const SECRET_KEY_NAME: &str = "SECRET_KEY";    //Secret Key name in env file 
+
+// // === ADD NEW APP HERE !!!
+// // Collect all app setting to one collection
+// pub fn app_setting_collection() -> Vec<AppSettings>{
+//     vec![
+//         crate::auth::get_app_settings(),
+//         // crate::new_app::get_app_settings(), // Uncomment and edit as needed
+//     ]
+// }
+
+
