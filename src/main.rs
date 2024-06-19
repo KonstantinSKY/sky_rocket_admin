8705098af4a858482c78dd5b7f964396ed28f9c5
SKY <sky012877@gmail.com>
2024-06-19 11:55:07 -0700

    src/main.rs - Modified :

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use rocket_db_pools::{Connection, Database};
+use std::env;
+
+#[derive(Database)]
+#[database("postgres")]
+pub struct DbConn(rocket_db_pools::diesel::PgPool);
+
+mod settings;
+
+#[rocket::main]
+async fn main() {
+    dotenv::dotenv().ok();
+      // Print DATABASE_URL to verify it is loaded
+      println!("DATABASE_URL: {:?}", env::var("DATABASE_URL"));
+    let _ = rocket::build()
+        .mount("/", rocket::routes![
+        ])
+        .attach(DbConn::init())
+        .launch()
+        .await;
 }
