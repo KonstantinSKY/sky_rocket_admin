3cd0422615bbf726e90fd1953022b00aa9a18e35
SKY <sky012877@gmail.com>
2024-06-19 15:25:36 -0700

    src/auth/mod.rs - Added :

---
+++ src/auth/mod.rs
@@ -0,0 +1,27 @@
+pub mod routes;
+pub mod models;
+// pub mod services;
+
+use crate::project::AppSettings;
+use rocket::routes;
+use routes as R;
+
+// Setting app application HERE
+pub fn get_app_settings() -> AppSettings {   
+    AppSettings { 
+        name : "auth", 
+        verbose_name: "Authentication",
+        routes: routes![
+          R::hi_json::hi_json,
+          R::hello::hello,
+          // R::users::get_all_users,
+          // R::users::add_user,
+          // R::users::delete_user,
+          // R::groups::get_all_groups,
+          // R::groups::add_group,
+          // R::groups::delete_group,
+          // R::user_group::get_all,
+          // R::user_group::add_one,
+          ],
+  } 
+}
