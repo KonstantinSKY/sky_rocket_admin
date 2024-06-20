096d29d909e719aa3ce66206cf349d8846d6a73a
SKY <sky012877@gmail.com>
2024-06-20 00:02:02 -0700

    src/auth/repositories/users.rs - Modified :

--- src/auth/repositories/users.rs
+++ src/auth/repositories/users.rs
@@ -30,6 +30,37 @@ impl UserRepository {
             .get_result(conn)
             .await
     }
+    // pub async fn create_user_with_groups(conn: &mut Conn, new_user: NewUser, role_codes: Vec<i32>) -> QueryResult<User> {
+    //     let user_with_hashed_password = NewUser {
+    //         password: crypto::hash_password(new_user.password),
+    //         ..new_user
+    //     };
+        
+    //     let user = diesel::insert_into(users::table)
+    //         .values(new_user)
+    //         .get_result::<User>(conn)
+    //         .await?;
+
+    //     for group_code in group_codes {
+    //         let new_user_role = {
+    //             if let Ok(role) = RoleRepository::find_by_code(c, &role_code).await {
+    //                 NewUserRole { user_id: user.id, role_id: role.id }
+    //             } else {
+    //                 let name = role_code.to_string();
+    //                 let new_role = NewRole { code: role_code, name };
+    //                 let role = RoleRepository::create(c, new_role).await?;
+    //                 NewUserRole { user_id: user.id, role_id: role.id }
+    //             }
+    //         };
+
+    //         diesel::insert_into(users_roles::table)
+    //             .values(new_user_role)
+    //             .get_result::<UserRole>(c)
+    //             .await?;
+    //     }
+
+    //     Ok(user)
+    // }
 
     pub async fn update(conn: &mut Conn, id: i32, user: UpdateUser) -> QueryResult<User> {
         diesel::update(users::table.find(id))
