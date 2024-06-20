c415a603141d3567bf4ade1ef3a76d3173710514
SKY <sky012877@gmail.com>
2024-06-19 18:32:02 -0700

    src/auth/models/users.rs - Modified :

--- src/auth/models/users.rs
+++ src/auth/models/users.rs
@@ -25,3 +25,47 @@ pub struct NewUser {
     pub email: String,
     pub password: String
 }
+
+#[derive(Insertable, Deserialize)]
+#[diesel(table_name=users)]
+pub struct UpdateUser {
+    pub username: String,
+    pub password: String,
+    pub email: String,
+    pub first_name: Option<String>,
+    pub last_name: Option<String>,
+    pub is_active: bool,
+    pub is_staff: bool,
+    pub is_superuser: bool,
+}
+
+#[derive(Serialize)]
+pub struct UserResponse {
+    pub id: i32,
+    pub username: String,
+    pub email: String,
+    pub first_name: Option<String>,
+    pub last_name: Option<String>,
+    pub created_at: chrono::NaiveDateTime,
+    pub last_login: Option<chrono::NaiveDateTime>,
+    pub is_active: bool,
+    pub is_staff: bool,
+    pub is_superuser: bool,
+}
+
+impl From<User> for UserResponse {
+    fn from(user: User) -> Self {
+        UserResponse {
+            id: user.id,
+            username: user.username,
+            email: user.email,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            created_at: user.created_at,
+            last_login: user.last_login,
+            is_active: user.is_active,
+            is_staff: user.is_staff,
+            is_superuser: user.is_superuser,
+        }
+    }
+}
