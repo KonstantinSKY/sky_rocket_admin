7f1492d3474a11da8cb846d31496d2a51e286447
SKY <sky012877@gmail.com>
2024-06-20 00:02:02 -0700

    src/auth/routes/mod.rs - Modified :

--- src/auth/routes/mod.rs
+++ src/auth/routes/mod.rs
@@ -1,5 +1,11 @@
 pub mod hello;
 pub mod hi_json;
 pub mod users;
-// pub mod groups;
+pub mod groups;
 // pub mod user_group;
+
+
+use rocket_db_pools::Connection;
+use crate::project::database::DbConn; 
+
+type Conn = Connection<DbConn>;
