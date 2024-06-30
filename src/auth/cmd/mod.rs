cbf78fd28f4a6bcc36db3bf51d378e9137af8208
SKY <sky012877@gmail.com>
2024-06-29 18:13:31 -0700

    src/auth/cmd/mod.rs - Added :

---
+++ src/auth/cmd/mod.rs
@@ -0,0 +1,47 @@
+mod superuser;
+use clap::{Command, Arg, ArgMatches};
+
+pub const CMD_NAME: &str = "auth";
+
+pub fn commands() -> Command {
+    Command::new(CMD_NAME)
+        .about("Auth CLI management")
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("superuser")
+                .about("Superuser management")
+                .subcommand(
+                    Command::new("create")
+                        .about("Create New Superuser")
+                        .arg_required_else_help(true)
+                        .arg(Arg::new("username").required(true))
+                        .arg(Arg::new("password").required(true)),
+                ),
+        )
+}
+
+pub async fn handle(matches: &ArgMatches) {
+    match matches.subcommand() {
+        Some(("superuser", sub_matches)) => {
+            match sub_matches.subcommand() {
+                Some(("create", create_matches)) => {
+                    // Call your create function here
+                    superuser::create(
+                        create_matches.get_one::<String>("username").unwrap().to_owned(),
+                        create_matches.get_one::<String>("password").unwrap().to_owned(),
+                    ).await;
+                },
+                _ => {
+                    eprintln!("Invalid subcommand for superuser");
+                }
+            }
+        },
+        _ => {
+            eprintln!("Invalid command");
+        }
+    }
+}
+
+
+
+
