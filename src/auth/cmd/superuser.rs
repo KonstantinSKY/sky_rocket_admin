b681dc426f964e7166b478471d0fdcb0cda8e4a0
SKY <sky012877@gmail.com>
2024-07-01 23:32:41 -0700

    src/auth/cmd/superuser.rs - Modified :

--- src/auth/cmd/superuser.rs
+++ src/auth/cmd/superuser.rs
@@ -1,11 +1,8 @@
-use diesel::result::DatabaseErrorKind;
 use diesel_async::{AsyncPgConnection, AsyncConnection};
 use dotenv::dotenv;
 use validator::Validate;
 
-use crate::auth::cmd::superuser;
 
-use super::super::services::auth;
 use super::super::repositories::users::UserRepository;
 use super::super::models::users::NewSuperUser;
 //TODO move to mod.rs
@@ -19,28 +16,9 @@ pub async fn load_db_connection() -> AsyncPgConnection {
 
 pub async fn create(username: String, email: String, password: String) {
     let mut conn = load_db_connection().await;
-    println!("Username: {}", username);
-    println!("Email: {}", email);
-    // println!("Password: {}", password);
-    
+    println!("Username: {username}");
+    println!("Email: {email}");
 
-    // match UserRepository::find_by_username(&mut conn, &username).await {
-    //     Ok(User) => {
-    //         println!("User with username {} already exists", username);
-    //         println!()
-    //         return;
-    //     }
-    //     Err(diesel::result::Error::NotFound) => {
-    //         println!("Creating user with username: {}", username);
-    //     }
-    //     Err(others) => {
-    //         println!("An error occurred: {:?}", others);
-    //         return;
-    //     }
-    // }
-
-    // let password_hash = auth::hash_password(password);
-    // println!("Hashed Password: {}", password_hash);
     let new_user = NewSuperUser{
         username,
         email,
@@ -48,38 +26,31 @@ pub async fn create(username: String, email: String, password: String) {
         is_staff: true,
         is_superuser: true,
     };
-    match  new_user.validate(){
-        Ok(_) => {
-            println!("Validation successful");
-            match UserRepository::create_superuser(&mut conn, new_user).await {
-                    Ok(user) => {
-                        println!("Added Super User with username: {}", user.username);
-                    }
-                    Err(e) => {
-                        println!("An error occurred: {:?}", e);
-                    }
-                }
-        }
-        Err(errors) => {
-            for (field, errors) in errors.field_errors() {
-                for error in errors {
-                    if let Some(message) = &error.message {
-                        println!("Validation error on {}: {}", field, message);
-                    } else {
-                        println!("Validation error on {}: {:?}", field, error);
-                    }
+
+    if let Err(validation_errors) = new_user.validate() {
+        for (field, errors) in validation_errors.field_errors() {
+            for error in errors {
+                if let Some(message) = &error.message {
+                    println!("Validation error on {field}: {message}");
+                } else {
+                    println!("Validation error on {field}: {error:?}");
                 }
             }
         }
+        return;
+    }
+
+    let new_user = match new_user.add_hashed_password() {
+        Ok(user) => user,
+        Err(e) => {
+            println!("Hashing error {e}");
+            return;
+        }
     };
-    // println!("new use {:?}",new_user);
-    // match UserRepository::create_superuser(&mut conn, new_user).await {
-    //     Ok(user) => {
-    //         println!("User with username {} added", user.username);
-    //     }
-    //     Err(e) => {
-    //         println!("An error occurred: {:?}", e);
-    //     }
-    // }
-    
+// 
+    let _ = UserRepository::create_superuser(&mut conn, new_user)
+        .await
+        .map(|user| println!("Created Superuser: {0}  with email: {1}", user.username, user.email))
+        .map_err(|e| println!("Superuser Not Created,  Error: {e}"));
+
 }
