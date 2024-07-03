e9f7fd021745966bdc27f2a037fdf9b77b1aa538
SKY <sky012877@gmail.com>
2024-07-03 12:21:15 -0700

    src/auth/routes/authorization.rs - Modified :

--- src/auth/routes/authorization.rs
+++ src/auth/routes/authorization.rs
@@ -1,10 +1,12 @@
 use crate::auth::services::jwt::get_jwt_token;
+use crate::project::ProjectSettings;
 
 use super::super::repositories::users::UserRepository;
 use super::Conn; //Import from mod.rs
 use rocket::http::Status;
 use rocket::response::status::Custom;
 use rocket::serde::json::{json, Json, Value};
+use rocket::State;
 use super::super::services;
 
 
@@ -16,7 +18,7 @@ pub struct Credentials {
 
 
 #[rocket::post("/login", format = "json", data = "<credentials>")]
-pub async fn login(mut db: Conn, credentials: Json<Credentials>) -> Result<Value, Custom<Value>> {
+pub async fn login(mut db: Conn, credentials: Json<Credentials>,  state: &State<ProjectSettings>) -> Result<Value, Custom<Value>> {
     println!("Credentials: {credentials:?}");
 
     let user = match UserRepository::find_by_username(&mut db, &credentials.username).await {
@@ -29,7 +31,7 @@ pub async fn login(mut db: Conn, credentials: Json<Credentials>) -> Result<Value
         return Err(Custom(Status::Unauthorized, json!("Wrong credentials")));
     }
 
-    match get_jwt_token(user.id, &user.username, &user.email, 3600) {
+    match get_jwt_token(user.id, &user.username, &user.email, 3600, state) {
         Ok(token) => Ok(json!({ "token": token })),
         Err(_) => Err(Custom(Status::InternalServerError, json!("Server Token Error")))
     }
