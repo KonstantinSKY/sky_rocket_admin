a672f4e24da8cbc292fcabd2e16f581ccbc50c95
SKY <sky012877@gmail.com>
2024-07-03 11:57:22 -0700

    src/project/mod.rs - Modified :

--- src/project/mod.rs
+++ src/project/mod.rs
@@ -3,12 +3,14 @@
 // pub mod validators;
 pub mod database;
 
-use std::env;
 use dotenv::dotenv;
+use std::env;
 
-use rocket::{figment::Figment, Config, Route};
-use crate::settings as project_setting;
-// use rocket_okapi::settings;
+use crate::{schema::groups::name, settings as project_setting};
+use rocket::Route;
+
+const PROJECT_NAME: &str = "Sky Rocket Admin";
+const PROJECT_VERBOSE_NAME: &str = "Sky Rocket Admin System";
 
 pub struct AppSettings {
     pub name: &'static str,
@@ -17,35 +19,52 @@ pub struct AppSettings {
 }
 
 pub struct ProjectSettings {
-    pub secret_key: String,  
-    pub figment: Figment,
+    pub name: String, 
+    pub verbose_name: String,
+    pub secret_key: String,
+}
+
+pub struct GlobalSettings {
+    pub project_settings: ProjectSettings, 
     pub apps_settings: Vec<AppSettings>,
     pub routes: Vec<Route>,
 }
 
-impl ProjectSettings {
-    pub fn new () -> Self {
-        dotenv().ok();      // Load environment variables
+impl GlobalSettings {
+    #[must_use]
+    pub fn new() -> Self {
+        dotenv().ok(); // Load environment variables
 
         //secret key
-        let secret_key = env::var(project_setting::SECRET_KEY_NAME).expect("SECRET KEY must be set");
-        let figment = Config::figment()   
-                                .merge(("secret_key", secret_key.clone()));
-        
+        let secret_key = env::var(project_setting::SECRET_KEY_NAME)
+            .map_err(|_| "SECRET KEY must be set. Set as default".to_string())
+            .unwrap_or_else(|_| "XXXX-DEFAULT_SECRET_KEY-YYYY".to_string());
+
+        let project_settings: ProjectSettings = ProjectSettings {
+            name: PROJECT_NAME.to_owned(),
+            verbose_name: PROJECT_VERBOSE_NAME.to_owned(),
+            secret_key,
+        };
+
         //app settings
         let apps_settings = project_setting::app_setting_collection();
 
-        // Collection app route        
+        // Collection app route
         let mut routes = Vec::<Route>::new();
-        for app_settings in &apps_settings{
+        for app_settings in &apps_settings {
             routes.extend(app_settings.routes.clone());
         }
-        
+
         Self {
-            secret_key,
-            figment,
+            project_settings,
             apps_settings,
             routes,
-        }        
+        }
+    }
+}
+
+impl Default for GlobalSettings {
+    fn default() -> Self {
+        Self::new()
     }
 }
